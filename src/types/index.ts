c7c9830c0f70db9358f0fead4378edab3e463781
Lucas <mazalucas@hotmail.com>
2023-09-06 01:17:19 -0300

    feat(types): added exam types
    Added exam types for available exams, enrollment process, enrollment turns and enrollment voiding.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 export * from './academic/academic.status.entry';
+
 export * from './persona/persona.types';
+
 export * from './polls/poll.entry';
+
+export * from './exam/exam.available';
+export * from './exam/exam.enroll';
+export * from './exam/exam.turn';
+export * from './exam/exam.void';
