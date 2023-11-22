ae4bbd18ec00db2f15d5535f9bee3d6d54c5d121
Lucas <mazalucas@hotmail.com>
2023-11-22 00:13:50 -0300

    feat(*): latest build

--- dist/packages/polling/polling.available.d.ts
+++ dist/packages/polling/polling.available.d.ts
@@ -1,4 +1,5 @@
 import { PollEntry } from "../../types/polls/poll.entry";
+import { PollQuestions } from "../../types/polls/poll.questions";
 import { HttpClient } from "../http/http.client";
 export interface IPollingAvailable {
     /**
@@ -9,10 +10,20 @@ export interface IPollingAvailable {
      * @returns {Promise<Array<PollEntry>>}
      */
     fetch(): Promise<Array<PollEntry>>;
+    /**
+     * Searches for all questions available for a specific poll entry.
+     *
+     * @param {PollEntry} pollEntry The poll entry to search for.
+     *
+     * @returns {Promise<PollQuestions>}
+     */
+    questions(pollEntry: PollEntry): Promise<PollQuestions>;
 }
 export declare class PollingAvailable implements IPollingAvailable {
     private readonly _http;
     private readonly _available;
+    private readonly _polls;
     constructor(_http: HttpClient);
     fetch(): Promise<Array<PollEntry>>;
+    questions(pollEntry: PollEntry): Promise<PollQuestions>;
 }
--- dist/packages/polling/polling.available.js
+++ dist/packages/polling/polling.available.js
@@ -4,12 +4,17 @@ exports.PollingAvailable = void 0;
 class PollingAvailable {
     _http;
     _available = "encuestas/encuestas-disponibles";
+    _polls = "encuestas/preguntas";
     constructor(_http) {
         this._http = _http;
     }
     async fetch() {
         const availablePolls = await this._http.request(this._available, "GET");
         return availablePolls;
+    }
+    async questions(pollEntry) {
+        const questions = await this._http.request(this._polls, "POST", {}, pollEntry);
+        return questions;
     }
 }
 exports.PollingAvailable = PollingAvailable;
